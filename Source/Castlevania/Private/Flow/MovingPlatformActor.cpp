d6367d12fb5dac9694468ce27f9b6daf8e1da11d
samalo0 <stephen.a.maloney@gmail.com>
2021-01-16 20:07:01 -0500

    Added moving platform. It will need to be more performant.

--- Source/Castlevania/Private/Flow/MovingPlatformActor.cpp
+++ Source/Castlevania/Private/Flow/MovingPlatformActor.cpp
@@ -6,9 +6,124 @@
 
 #include "Flow/MovingPlatformActor.h"
 
+#include "Components/BoxComponent.h"
+#include "CastlevaniaPawn.h"
+#include "PaperSpriteComponent.h"
+
 AMovingPlatformActor::AMovingPlatformActor()
 {
 	PrimaryActorTick.bCanEverTick = true;
 	PrimaryActorTick.bStartWithTickEnabled = false;
+
+	RootSceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootSceneComponent"));
+	SetRootComponent(RootSceneComponent);
+	RootSceneComponent->SetMobility(EComponentMobility::Static);
+	
+	BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent"));
+	BoxComponent->SetupAttachment(GetRootComponent());
+	BoxComponent->SetMobility(EComponentMobility::Movable);
+	BoxComponent->SetCollisionProfileName("OverlapOnlyPawn");
+			
+	SpriteComponent = CreateDefaultSubobject<UPaperSpriteComponent>(TEXT("SpriteComponent"));
+	SpriteComponent->SetupAttachment(BoxComponent);
+	SpriteComponent->SetCollisionProfileName("BlockAll");
+	SpriteComponent->SetGenerateOverlapEvents(false);
+	SpriteComponent->SetMobility(EComponentMobility::Movable);
 }
+
+void AMovingPlatformActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	InitialWorldLocation = GetActorLocation();
+	
+	SecondaryWorldLocation = (SecondaryRelativeTransform * GetActorTransform()).GetLocation();
+	
+	bSecondaryIsOnRight = InitialWorldLocation.X < SecondaryWorldLocation.X;
+
+	bHeadingRight = bSecondaryIsOnRight;
+	
+	Acceleration = MaximumVelocity / TurnAroundTime;
+
+	BoxComponent->OnComponentBeginOverlap.AddDynamic(this, &AMovingPlatformActor::OnBoxBeginOverlap);
+	BoxComponent->OnComponentEndOverlap.AddDynamic(this, &AMovingPlatformActor::OnBoxEndOverlap);
+	
+	SetActorTickEnabled(true);
+}
+
+void AMovingPlatformActor::OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	ACastlevaniaPawn* Pawn = Cast<ACastlevaniaPawn>(OtherActor);
+	if(IsValid(Pawn))
+	{
+		Pawn->AttachToComponent(BoxComponent, FAttachmentTransformRules::KeepWorldTransform);
+	}
+}
+
+void AMovingPlatformActor::OnBoxEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	ACastlevaniaPawn* Pawn = Cast<ACastlevaniaPawn>(OtherActor);
+	if(IsValid(Pawn))
+	{
+		Pawn->DetachFromActor(FDetachmentTransformRules::KeepWorldTransform);
+	}
+}
+
+void AMovingPlatformActor::Tick(const float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	const FVector CurrentLocation = BoxComponent->GetComponentLocation();
+
+	FVector DesiredLocation;
+	FVector StartingLocation;
+	if(bSecondaryIsOnRight)
+	{
+		DesiredLocation = bHeadingRight? SecondaryWorldLocation : InitialWorldLocation;
+		StartingLocation = bHeadingRight? InitialWorldLocation : SecondaryWorldLocation;
+	}
+	else
+	{
+		DesiredLocation = bHeadingRight? InitialWorldLocation : SecondaryWorldLocation;
+		StartingLocation = bHeadingRight? SecondaryWorldLocation : InitialWorldLocation;
+	}
+
+	const bool bStarting = FVector::Dist(CurrentLocation, StartingLocation) <= TurnAroundDistance;
+	const bool bTurningAround = FVector::Dist(CurrentLocation, DesiredLocation) <= TurnAroundDistance;
+
+	if((bHeadingRight && bTurningAround) ||
+		(!bHeadingRight && bStarting))
+	{
+		Velocity -= Acceleration * DeltaSeconds;
+		if(Velocity < -1.0f * MaximumVelocity)
+		{
+			Velocity = -1.0f * MaximumVelocity;
+		}
+	}
+	else if((!bHeadingRight && bTurningAround) ||
+		(bHeadingRight && bStarting))
+	{
+		Velocity += Acceleration * DeltaSeconds;
+		if(Velocity > MaximumVelocity)
+		{
+			Velocity = MaximumVelocity;
+		}
+	}	
+
+	const FVector NewLocation = FVector(CurrentLocation.X + Velocity * DeltaSeconds, CurrentLocation.Y, CurrentLocation.Z);
+	BoxComponent->SetWorldLocation(NewLocation);
+
+	FVector RoundLocation = SpriteComponent->GetComponentLocation();
+	RoundLocation.X = FMath::RoundToInt(NewLocation.X);
+	SpriteComponent->SetWorldLocation(RoundLocation);
+
+	if((bHeadingRight && NewLocation.X >= DesiredLocation.X) ||
+		(!bHeadingRight && NewLocation.X <= DesiredLocation.X))
+	{
+		bHeadingRight = !bHeadingRight;
+	}
+}
+
 
--- Source/Castlevania/Public/Flow/MovingPlatformActor.h
+++ Source/Castlevania/Public/Flow/MovingPlatformActor.h
@@ -10,7 +10,10 @@
 #include "GameFramework/Actor.h"
 #include "MovingPlatformActor.generated.h"
 
-UCLASS()
+class UBoxComponent;
+class UPaperSpriteComponent;
+
+UCLASS(HideCategories = (Replication, Rendering, Actor, Input, LOD, Cooking))
 class CASTLEVANIA_API AMovingPlatformActor : public AActor
 {
 
@@ -20,4 +23,59 @@ public:
 
 	AMovingPlatformActor();
 
+	virtual void Tick(float DeltaSeconds) override;
+	
+protected:
+
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnBoxBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnBoxEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	
+	UPROPERTY(Transient, VisibleInstanceOnly)
+	float Acceleration;
+
+	UPROPERTY(VisibleInstanceOnly)
+	bool bHeadingRight = false;
+	
+	UPROPERTY(Transient, VisibleInstanceOnly)
+	FVector InitialWorldLocation;
+
+	UPROPERTY(Transient, VisibleInstanceOnly)
+	float MaximumVelocity = 16.0f;
+
+	UPROPERTY(VisibleInstanceOnly)
+	bool bSecondaryIsOnRight = false;
+	
+	UPROPERTY(EditAnywhere, meta = (MakeEditWidget = true))
+	FTransform SecondaryRelativeTransform;
+
+	UPROPERTY(Transient, VisibleInstanceOnly)
+	FVector SecondaryWorldLocation;
+
+	UPROPERTY(EditAnywhere)
+	float TurnAroundDistance = 8.0f;
+	
+	UPROPERTY(EditAnywhere)
+	float TurnAroundTime = 0.5f;
+
+	UPROPERTY(VisibleInstanceOnly)
+	float Velocity = 0.0f;
+	
+#pragma region Components
+
+	UPROPERTY(VisibleAnywhere)
+	UBoxComponent* BoxComponent;
+	
+	UPROPERTY(VisibleAnywhere)
+	UPaperSpriteComponent* SpriteComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* RootSceneComponent;
+	
+#pragma endregion
+	
 };
