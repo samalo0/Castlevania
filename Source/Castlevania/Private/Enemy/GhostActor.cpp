47cd4b3927defad7ad0911c73810338fb12f9696
samalo0 <stephen.a.maloney@gmail.com>
2021-01-19 22:46:06 -0500

    Added ghost and ghost respawner.

--- Source/Castlevania/Private/Enemy/GhostActor.cpp
+++ Source/Castlevania/Private/Enemy/GhostActor.cpp
@@ -6,3 +6,67 @@
 
 #include "Enemy/GhostActor.h"
 
+
+#include "CastlevaniaFunctionLibrary.h"
+#include "CastlevaniaGameModeBase.h"
+#include "CastlevaniaPawn.h"
+#include "PaperFlipbookComponent.h"
+
+AGhostActor::AGhostActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+	
+	DamageToPlayer = 3;
+	Life = 4;
+	Score = 300;
+}
+
+void AGhostActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if(IsValid(GameMode))
+	{
+		Pawn = GameMode->GetPlayerPawn();
+		SetActorTickEnabled(true);
+	}
+
+	LocationFloat = GetActorLocation();
+}
+
+void AGhostActor::HitWithWeapon(const int32 Damage, const bool bPlaySound, const FVector WeaponLocation)
+{
+	Super::HitWithWeapon(Damage, bPlaySound, WeaponLocation);
+
+	if(Life <= 0)
+	{
+		SetActorTickEnabled(false);
+	}
+}
+
+void AGhostActor::Tick(const float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	if(!IsValid(Pawn))
+	{
+		SetActorTickEnabled(false);
+	}
+
+	LocationFloat = FMath::VInterpConstantTo(LocationFloat, Pawn->GetActorLocation(), DeltaSeconds, InterpolationSpeed);
+	const FVector LocationInteger = UCastlevaniaFunctionLibrary::RoundVectorToInt(LocationFloat);
+	if(!GetActorLocation().Equals(LocationInteger, 0.99f))
+	{
+		SetActorLocation(LocationInteger);
+
+		if(Pawn->GetActorLocation().X < LocationInteger.X)
+		{
+			FlipbookComponent->SetWorldScale3D(FVector(-1.0f, 1.0f, 1.0f));
+		}
+		else
+		{
+			FlipbookComponent->SetWorldScale3D(FVector(1.0f, 1.0f, 1.0f));
+		}
+	}
+}
--- Source/Castlevania/Public/Enemy/GhostActor.h
+++ Source/Castlevania/Public/Enemy/GhostActor.h
@@ -10,6 +10,8 @@
 #include "Enemy/EnemyActor.h"
 #include "GhostActor.generated.h"
 
+class ACastlevaniaPawn;
+
 /**
  * Child enemy that is a ghost, which follows the player slowly.
  */
@@ -18,5 +20,25 @@ class CASTLEVANIA_API AGhostActor : public AEnemyActor
 {
 
 	GENERATED_BODY()
+
+public:
+
+	AGhostActor();
+
+	virtual void HitWithWeapon(int32 Damage, bool bPlaySound, const FVector WeaponLocation) override;
+	
+	virtual void Tick(float DeltaSeconds) override;
+	
+protected:
+
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly)
+	float InterpolationSpeed = 40.0f;
+
+	FVector LocationFloat;
+	
+	UPROPERTY(Transient)
+	ACastlevaniaPawn* Pawn; 
 	
 };
