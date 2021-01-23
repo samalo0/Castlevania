3e752d486ff81276afe8936f4bc1c575f7b2e3f2
samalo0 <stephen.a.maloney@gmail.com>
2021-01-23 17:37:33 -0500

    Added medusa boss, snakes base classes; they have no functionality as of yet.

---
+++ Source/Castlevania/Public/Enemy/MedusaActor.h
@@ -0,0 +1,22 @@
+//----------------------------------------------------------------------------------------------------------------------
+// Copyright 2021 Stephen Maloney
+// Owner: Stephen Maloney
+// Created: 1/23/2021
+//----------------------------------------------------------------------------------------------------------------------
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy/EnemyActor.h"
+#include "MedusaActor.generated.h"
+
+/**
+ * Child class of enemy for the Medusa boss.
+ */
+UCLASS()
+class CASTLEVANIA_API AMedusaActor : public AEnemyActor
+{
+
+	GENERATED_BODY()
+	
+};
---
+++ Source/Castlevania/Public/Enemy/MedusaSnakeActor.h
@@ -0,0 +1,22 @@
+//----------------------------------------------------------------------------------------------------------------------
+// Copyright 2021 Stephen Maloney
+// Owner: Stephen Maloney
+// Created: 1/23/2021
+//----------------------------------------------------------------------------------------------------------------------
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy/EnemyActor.h"
+#include "MedusaSnakeActor.generated.h"
+
+/**
+ * Snakes spawned by the Medusa boss.
+ */
+UCLASS()
+class CASTLEVANIA_API AMedusaSnakeActor : public AEnemyActor
+{
+
+	GENERATED_BODY()
+	
+};
